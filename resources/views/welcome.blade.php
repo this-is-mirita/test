05c0a9abc58801a54ea4f4f93c71b3b1949ba188
mirita <popap182@gmail.com>
2025-05-03 21:14:26 +0300

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    <title>React + Laravel</title>
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
